c5b1c5e7464e6f8aae046c31d51422f16a3674d8
wardymate <chriswarddev@gmail.com>
2015-06-01 21:38:24 +0100

    can add a new idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,4 +3,17 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
   end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    Idea.create(idea_params)
+    redirect_to '/ideas'
+  end
+
+  def idea_params
+    params.require(:idea).permit(:name)
+  end
 end
--- spec/features/ideas_feature_spec.rb
+++ spec/features/ideas_feature_spec.rb
@@ -10,14 +10,25 @@ feature 'ideas' do
   end
 
   context 'ideas have been added' do
-  before do
-    Idea.create(name: 'Lions tour')
+    before do
+      Idea.create(name: 'Lions tour')
+    end
+
+    scenario 'display ideas' do
+      visit '/ideas'
+      expect(page).to have_content('Lions tour')
+      expect(page).not_to have_content('No ideas yet')
+    end
   end
 
-  scenario 'display ideas' do
-    visit '/ideas'
-    expect(page).to have_content('Lions tour')
-    expect(page).not_to have_content('No ideas yet')
+  context 'creating ideas' do
+    scenario 'prompts user to fill out a form, then displays the new idea' do
+      visit '/ideas'
+      click_link 'Add an idea'
+      fill_in 'Name', with: 'Lions tour'
+      click_button 'Create Idea'
+      expect(page).to have_content 'Lions tour'
+      expect(current_path).to eq '/ideas'
+    end
   end
-end
 end
