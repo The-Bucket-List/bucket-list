f1ff2c61062ef0bce9bc10e0b71e092cf93ab4da
wardymate <chriswarddev@gmail.com>
2015-06-01 18:17:23 +0100

    add ideas index page

---
+++ spec/features/ideas_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'ideas' do
+  context 'no ideas have been added' do
+    scenario 'should display a prompt to add a new idea' do
+      visit 'ideas'
+      expect(page).to have_content 'No ideas yet'
+      expect(page).to have_link 'Add an idea'
+    end
+  end
+end
